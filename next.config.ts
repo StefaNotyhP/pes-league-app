b4c48f2a17bc16eaf16e371ab63a8e69748911a3
StefaNotyhP <103865357+StefaNotyhP@users.noreply.github.com>
2026-02-24 14:07:30 +0100

    Simplify next.config.ts by removing origins
    Removed experimental allowedDevOrigins configuration.

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  experimental: {
-    allowedDevOrigins: [
-      "https://nonfloating-dung-violaceous.ngrok-free.dev",
-      "http://localhost:3000",
-      "http://192.168.1.4:3000",
-    ],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
